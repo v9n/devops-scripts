421db7b12c442be92a7dc42c327bc8cd8753ff09
Vinh Nguyen <kurei@axcoto.com>
2016-08-24 00:19:23 -0700

    Finally make restore work

--- cassandra/restore.sh
+++ cassandra/restore.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -eu
+set -o pipefail
+
 HOSTNAME=`hostname`
 DATA_DIR="/ebs/data"
 S3BUCKET="###"
-
 
 # https://docs.datastax.com/en/cassandra/3.x/cassandra/operations/opsSnapshotRestoreNewCluster.html?hl=restoring,snapshot,new,cluste
 restore_token () {
@@ -22,13 +24,22 @@ restore_keyspace () {
     for table in `aws s3 ls s3://$S3BUCKET/$node$KEYSPACE/ | awk '{print $2}'`; do
       table_list+=("$table")
 
+      if [ ! -d $DATA_DIR/$KEYSPACE/$table ]; then
+        mkdir $DATA_DIR/$KEYSPACE/$table || true
+      fi
+
       echo "    Table: $table"
       latest_snapshot=`aws s3 ls s3://$S3BUCKET/$node$KEYSPACE/$table | tail -n1 | awk '{print $2}'`
       echo "    Latest: $latest_snapshot"
 
-      echo aws s3 ls s3://$S3BUCKET/$node$KEYSPACE/$table$latest_snapshot
+      echo "      -> list table aws s3 ls s3://$S3BUCKET/$node$KEYSPACE/$table$latest_snapshot"
       for sstable in `aws s3 ls s3://$S3BUCKET/$node$KEYSPACE/$table$latest_snapshot | grep 'ma-' | awk '{print $4}'`; do
         echo "      -> sstable: $sstable"
+        local node_number=`echo $node | sed "s/[^0-9]//g"`
+        sstable_new_name=`echo $sstable | sed "s/ma-/ma-$node_number/g"`
+        echo "      ->" Copy sstable: aws s3 cp s3://$S3BUCKET/$node$KEYSPACE/$table$latest_snapshot$sstable $DATA_DIR/$KEYSPACE/$table$sstable_new_name
+        # Temp disable for test
+        #aws s3 cp s3://$S3BUCKET/$node$KEYSPACE/$table$latest_snapshot$sstable $DATA_DIR/$KEYSPACE/$table$sstable_new_name
       done
     done
 
@@ -36,9 +47,11 @@ restore_keyspace () {
   done
 
   for t in "${table_list[@]}"; do
-  #for t in $table_list; do
     t=`echo $t | sed  "s/\// /g"`
     echo "  -> Table: $t"
+    local ip=`ip addr show eth0 | grep -Po 'inet \K[\d.]+'`
+    echo "  -> " sstableloader -d $ip $DATA_DIR/$KEYSPACE/$t
+    sstableloader -d $ip $DATA_DIR/$KEYSPACE/$t
   done
 }
 
@@ -58,4 +71,4 @@ main () {
   restore_data $1
 }
 
-main
+main $1
